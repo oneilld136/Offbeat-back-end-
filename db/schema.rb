4539d0f9389a6d109d4c629029f12e7b18a87045
github username <github email address>
2019-08-07 18:35:16 -0400

    first

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_07_215136) do
+ActiveRecord::Schema.define(version: 2019_08_07_220858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2019_08_07_215136) do
     t.string "imgURL"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type"
   end
 
   create_table "trips", force: :cascade do |t|
